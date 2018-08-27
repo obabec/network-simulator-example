0bc153818b35172e6043fde92d1b628e926b3f18
obabec <ond.babec@gmail.com>
2018-08-27 08:28:28 +0200

    Repaired CMD
    Signed-off-by: obabec <ond.babec@gmail.com>

---
+++ src/main/java/com/redhat/patriot/network_simulator/example/container/config/AppConfig.java
@@ -0,0 +1,55 @@
+package com.redhat.patriot.network_simulator.example.container.config;
+
+public class AppConfig {
+    private String ipadd;
+    private String status;
+    private String name;
+    private String startCommand;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartCommand() {
+        return startCommand;
+    }
+
+    public void setStartCommand(String startCommand) {
+        this.startCommand = startCommand;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    public AppConfig(String name, String startCommand) {
+        this.name = name;
+        this.startCommand = startCommand;
+    }
+
+    public AppConfig(String ipadd, String status, String name) {
+        this.ipadd = ipadd;
+        this.status = status;
+        this.name = name;
+    }
+    public AppConfig(String name) {
+        this.name = name;
+    }
+
+    public String getIpadd() {
+        return ipadd;
+    }
+
+    public void setIpadd(String ipadd) {
+        this.ipadd = ipadd;
+    }
+}
